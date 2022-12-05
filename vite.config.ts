7f827eac108fdd2aaf8a22a99e90f4bd9decea15
丁磊 <XNBigDevil@gmail.com>
2022-12-05 12:43:50 +0800

    🐳 chore: add vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import solid from 'solid-start/vite'
 import UnocssPlugin from '@unocss/vite'
 import mdx from '@mdx-js/rollup'
+import vercel from 'solid-start-vercel'
 
 export default defineConfig({
   plugins: [
     mdx({ jsxImportSource: 'solid-jsx' }),
-    solid(),
+    solid({ adapter: vercel() }),
     UnocssPlugin({
       // your config or in uno.config.ts
     })
