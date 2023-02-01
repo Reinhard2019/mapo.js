99c916acf2f69188550ee2cbaeab718acdff62f3
丁磊 <XNBigDevil@gmail.com>
2023-02-01 19:44:04 +0800

    🦄 refactor: remove react-to-solid-component package

--- packages/react-to-solid-component/src/index.ts
+++
@@ -1,4 +0,0 @@
-import ReactToSolid, { reactToSolid } from './ReactToSolid'
-import SolidToReact, { solidToReact } from './SolidToReact'
-
-export { ReactToSolid, reactToSolid, SolidToReact, solidToReact }
