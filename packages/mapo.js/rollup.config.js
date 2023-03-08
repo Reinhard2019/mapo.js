9fde5ec96855f58cd3797b8aa237f11ecc51f592
丁磊 <XNBigDevil@gmail.com>
2023-03-08 17:20:03 +0800

    🐳 chore(mapo.js): update rollup.config.js, add output.format: iife

--- packages/mapo.js/rollup.config.js
+++ packages/mapo.js/rollup.config.js
@@ -1,16 +1,46 @@
 import ts from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'esm/index.js',
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'esm/index.js',
+    },
+    plugins: [
+      ts({
+        tsconfig: 'tsconfig.json',
+      }),
+      commonjs(),
+    ],
+    external: ['lodash-es', 'three', '@turf/turf'],
   },
-  plugins: [
-    ts({
-      tsconfig: 'tsconfig.json',
-    }),
-    commonjs(),
-  ],
-  external: ['lodash-es', 'three', '@turf/turf', 'three/examples/jsm/libs/stats.module.js'],
-}
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'Mapo',
+      file: 'dist/index.js',
+      format: 'iife',
+      globals: {
+        three: 'THREE',
+      },
+    },
+    context: 'window',
+    plugins: [
+      ts({
+        tsconfig: 'tsconfig.json',
+        tsconfigOverride: {
+          compilerOptions: {
+            declaration: false,
+          },
+        },
+      }),
+      nodeResolve({
+        browser: true,
+      }),
+      commonjs(),
+    ],
+    external: ['three'],
+  },
+]
