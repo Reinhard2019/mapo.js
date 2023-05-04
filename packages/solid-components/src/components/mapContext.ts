4030343edd1556595a2f1e278bfc1ca6d255f125
丁磊 <XNBigDevil@gmail.com>
2023-05-04 16:49:59 +0800

    🦄 refactor: remove solid-component

--- packages/solid-components/src/components/mapContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'solid-js'
-import type { Accessor } from 'solid-js'
-import type { Map } from 'mapo.js'
-
-export const MapContext = createContext<{
-  map?: Accessor<Map | undefined>
-}>({})
