92d81cd9517cf6c4093c76ae97dbbed687c4b51f
丁磊 <XNBigDevil@gmail.com>
2023-12-07 16:24:26 +0800

    🐞 fix(mapo.js): TileMaterial 精度问题

--- packages/mapo.js/src/TileMaterial.ts
+++ packages/mapo.js/src/TileMaterial.ts
@@ -1,19 +1,57 @@
 import * as THREE from 'three'
 import { XYZ } from './types'
+import MercatorTile from './utils/MercatorTile'
 
 const vertexShader = `
 varying vec2 vUv;
+varying vec2 vLngLat;
+attribute vec2 lngLat;
 
 void main() {
   vUv = uv;
+  vLngLat = lngLat;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 `
+const lngToX = `
+float lngToX(float lng, float z) {
+  return ((lng + 180.0) / 360.0) * pow(2.0, z);
+}
+`
+// 将 lat(纬度) 转化为墨卡托投影中的 y
+const latToY = `
+float latToY(float lat, float z) {
+  float z2 = pow(2.0, z);
+
+  if (lat >= ${MercatorTile.maxLat}) {
+    return 0.0;
+  }
+  if (lat <= ${-MercatorTile.maxLat}) {
+    return z2;
+  }
+
+  float sinValue = sin(radians(lat));
+  float y = z2 * (0.5 - (0.25 * log((1.0 + sinValue) / (1.0 - sinValue))) / ${Math.PI});
+  return y;
+}
+`
+// TODO 精度问题，zoom 大于 15 时使用 lngToX 计算 uv 时会开始模糊，暂时直接使用 vUv.y
 const fragmentShader = `
 varying vec2 vUv;
+varying vec2 vLngLat;
 uniform sampler2D canvasTexture;
+uniform vec3 xyz;
+
+${lngToX}
+
+${latToY}
+
 void main() {
-  gl_FragColor = texture2D(canvasTexture, vUv);
+  float uvY = vUv.y;
+  if (xyz.z <= 15.0) {
+    uvY = 1.0 - (latToY(vLngLat.y, xyz.z) - xyz.y);
+  }
+  gl_FragColor = texture2D(canvasTexture, vec2(vUv.x, uvY));
 }
 `
 
